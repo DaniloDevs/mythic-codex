1bd8b7283930a9ec39b7a2d093ccff1e0d9dbb0c
Danilo Romão <https.sw@gmail.com>
2025-11-17 12:36:49 -0300

    Feat: add get character by id

---
+++ src/services/character/get-character-by-id.ts
@@ -0,0 +1,29 @@
+
+
+import type { Character } from "@/@types/character";
+import { ResourceNotFoundError } from "@/_errors/resource-not-found";
+import type { ICharacterRepository } from "@/repository/character-repository";
+
+interface RequestData {
+   id: string;
+}
+
+interface ResponseData {
+   character: Character;
+}
+
+export class GetCharacterByIdService {
+   constructor(
+      private characterRepository: ICharacterRepository,
+   ) { }
+
+   async execute({ id }: RequestData): Promise<ResponseData> {
+      const character = await this.characterRepository.getById(id);
+
+      if (!character) {
+         throw new ResourceNotFoundError();
+      }
+
+      return { character };
+   }
+}
